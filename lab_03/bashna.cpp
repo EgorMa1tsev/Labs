02954c477902aef6d7189f4176c099232ecf1130
EgorM <root@DESKTOP-7RFVCBH>
2024-08-15 15:27:40 +0300

    lab_03 task_2

---
+++ lab_03/bashna.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <list>
+
+void Solver(char a, char b, char c, int n, std::list<std::string>& actions) {
+    if (n == 1) {
+        std::string str;
+        str += a;
+        str += " -> ";
+        str += b;
+        actions.push_back(str);
+    } else {
+        Solver(a, c, b, n - 1, actions);
+        std::string str;
+        str += a;
+        str += " -> ";
+        str += b;
+        actions.push_back(str);
+        Solver(c, b, a, n - 1, actions);
+    }
+        
+}
+
+int main() {
+    std::list<std::string> actions;
+    int n;
+    std::cin >> n;
+    Solver('A', 'C', 'B', n, actions);
+    for (std::string act : actions) {
+        std::cout << act << "\n";
+    }
+}
